7a407bae2ba1b6a62e6d3d43324f51d66405b91e
Sam Rutcovschi <samuil.rutcovschi@gmail.com>
2022-11-21 15:28:35 +0200

    lab 5 - add CustomScrollView for all widgets of home page

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import '../../resources/custom_colors.dart';
 import '../../resources/strings.dart';
 import 'controller/home_controller.dart';
+import 'widgets/daily_exercise_widget/daily_exercise.dart';
 import 'widgets/daily_exercise_widget/daily_exercises_list.dart';
 import 'widgets/goal_carousel_widget/goal_carousel_widget.dart';
 import 'widgets/header_widget.dart';
@@ -29,6 +30,8 @@ class _HomePageState extends State<HomePage> {
 
     final double statusBarHeight = MediaQuery.of(context).viewPadding.top;
 
+    const Key centerKey = ValueKey<String>('bottom-sliver-list');
+
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: SystemUiOverlayStyle.dark, // play with this
       child: Scaffold(
@@ -36,9 +39,51 @@ class _HomePageState extends State<HomePage> {
             color: CustomColors.cultured,
             //decoration: BoxDecoration(border: Border.all(color: Colors.red, width: 3),),
 
-            child: Column(
-              children: [
-                SizedBox(height: statusBarHeight),
+            child: CustomScrollView(
+              //center: centerKey,
+              slivers: <Widget>[
+
+                SliverToBoxAdapter(child: SizedBox(height: statusBarHeight),),
+
+                const SliverToBoxAdapter(child: HeaderWidget(title: Strings.startNewGoal),),
+
+                SliverFixedExtentList(
+                  itemExtent: 281.0,
+                  delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) {
+                      return Obx( () => controller.goalItems.isNotEmpty
+                          ? GoalCarouselWidget(goals: controller.goalItems)
+                          : const SizedBox(width: 30, height: 300, child: CircularProgressIndicator(),)
+                      );
+                    },
+                    childCount: 1
+                  ),
+                ),
+
+                const SliverToBoxAdapter(child: HeaderWidget(title: Strings.dailyTask),),
+
+                SliverList(
+                  //itemExtent: 370.0,
+                  delegate: SliverChildBuilderDelegate(
+                          (BuildContext context, int i) {
+                        if(controller.exerciseItems.isNotEmpty) {
+                          var exercise = controller.exerciseItems[i];
+                          return Obx( () => Column(
+                            children: [
+                              DailyExercise(exercise: controller.exerciseItems[i]),
+                              const SizedBox(height: 12),
+                            ],
+                          ));
+                        } else {
+                          return const SizedBox(width: 30, height: 300, child: CircularProgressIndicator(),);
+                        }
+                      },
+                      childCount: controller.exerciseItems.isNotEmpty ? controller.exerciseItems.length : 1
+                  ),
+                ),
+
+
+                /*SizedBox(height: statusBarHeight),
 
                 const HeaderWidget(title: Strings.startNewGoal),
 
@@ -47,16 +92,16 @@ class _HomePageState extends State<HomePage> {
                     : const SizedBox(width: 30, height: 300,
                         child: CircularProgressIndicator(),
                       )
-                ),
+                ),*/
 
-                const HeaderWidget(title: Strings.dailyTask),
+                /*const HeaderWidget(title: Strings.dailyTask),
 
                 Obx( () => controller.exerciseItems.isNotEmpty
                     ? DailyExercisesList(exercises: controller.exerciseItems)
                     : const SizedBox(width: 30, height: 300,
                         child: CircularProgressIndicator(),
                       )
-                ),
+                ),*/
 
               ],
             ),
