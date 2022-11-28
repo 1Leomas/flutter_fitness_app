60e58ed2ad36c98b9b016ed65760cfbdec9fdb73
Sam Rutcovschi <samuil.rutcovschi@gmail.com>
2022-11-28 14:42:03 +0200

    data layer - add api datasource for goals

--- data/lib/fitness/data_sources/api/fitness_api_data_source.dart
+++ data/lib/fitness/data_sources/api/fitness_api_data_source.dart
@@ -4,9 +4,12 @@ import 'package:http/http.dart' as http;
 
 import '../../entities/api/exercise_api_dto.dart';
 import '../../entities/api/exercises_response_api_dto.dart';
+import '../../entities/api/goal_api_dto.dart';
+import '../../entities/api/goals_response_api_dto.dart';
 
 abstract class FitnessApiDataSource {
   Future<List<ExerciseApiDto>> getExercises();
+  Future<List<GoalApiDto>> getGoals();
 }
 
 class FitnessApiDataSourceImpl implements FitnessApiDataSource {
@@ -25,6 +28,19 @@ class FitnessApiDataSourceImpl implements FitnessApiDataSource {
     if (response.statusCode == 200) {
       ExercisesResponseApiDto exercisesResponse = ExercisesResponseApiDto.fromJson(jsonDecode(response.body));
       return exercisesResponse.exercises;
+    }
+    return [];
+  }
+
+  @override
+  Future<List<GoalApiDto>> getGoals() async {
+    Uri uri = Uri.parse('https://fitness-app-api.k8s.devebs.net/workout/goal');
+
+    var response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      GoalsResponseApiDto exercisesResponse = GoalsResponseApiDto.fromJson(jsonDecode(response.body));
+      return exercisesResponse.goals;
     }
     return [];
   }
